4d06ded6f50d2cb290ff3a109c9da38e325851e1
shyam sankar P <shyamsankarp539@gmail.com>
2025-04-22 05:54:51 +0530

    done

--- src/components/intro/Introduction.jsx
+++ src/components/intro/Introduction.jsx
@@ -1,37 +1,33 @@
-import React from 'react';
-import backgroundImage from '../../assets/bg.jpg';
-import Logo from '../../assets/logo.png';
+import React from 'react'
+import backgroundImage from '../../assets/bg.jpg'
+import Logo from '../../assets/hero.jpg'
 import { useNavigate } from "react-router-dom";
-import '../../styles/button.css';
-
+import '../../styles/button.css'
 const Introduction = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const handleClick = () => {
-    navigate("/login");
-  };
-
+  const handleClick = () =>{
+    navigate("/login")
+  }
   return (
     <div
-      className="intro-container"
+      className="position-relative w-100 vh-100 bg-dark"
       style={{
         backgroundImage: `url(${backgroundImage})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
-      }}
-    >
-       <div className="intro-overlay">
-       <h1 className="text-white text-5xl sm:text-6xl md:text-7xl font-extrabold mb-4 tracking-wide drop-shadow-lg">
-          GoTicket
-        </h1>
-        <p className="intro-tagline">Your number one movie destination.</p>
-        <button className="btn-custom" onClick={handleClick}>
+      }}>
+      <div className="position-absolute top-0 start-0 w-100 h-100 bg-black bg-opacity-75 d-flex flex-column align-items-center justify-content-center text-center p-4">
+        <img src={Logo} width="300px" alt="Logo" />
+        <p className="text-white fs-4">Your number one movie destination.</p>
+        <button
+          className="btn btn-danger  mt-4"
+          onClick={handleClick}>
           Watch Movies
         </button>
       </div>
     </div>
   );
-};
+}
 
-export default Introduction;
-
+export default Introduction
